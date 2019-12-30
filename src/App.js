7cb5793201062bcf795e967123b61dbebe4aa7da
CodeLyoko2610 <dangchuong1999@outlook.com>
2019-12-30 13:07:10 +0200

    Create component Todos and import it to the App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+//import components
+import Todos from './components/Todos';
+
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Todos />
     </div>
   );
 }
